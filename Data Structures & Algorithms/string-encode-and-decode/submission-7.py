28ce4e5ba0036bf9334470aa6dec1d57b8a1da6d
burrito227 <132732268+burrito227@users.noreply.github.com>
2026-06-22 18:47:05 -0500

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        return "".join(f"{len(s)}_{s}" for s in strs)
+
+    def decode(self, s: str) -> List[str]:
+        out = []
+
+        length = 0
+        start = 0
+        end = 0
+
+        while start < len(s):
+            end = s.find('_', start)
+            length = int(s[start:end])
+
+            substring = s[end + 1:end + length + 1]
+            out.append(substring)
+
+            start = end + length + 1
+
+        return out
